511e7a7f35cd899f362f6cbd85a68cc264a913f0
Grant Barkway <grantbarkway@shaw.ca>
2025-12-23 20:05:52 -0800

    Wrote day 5 part 1 functionally!

--- AdventOfCodeDay5/Part1/day5part1.ts
+++ AdventOfCodeDay5/Part1/day5part1.ts
@@ -1,37 +1,32 @@
 import * as fs from 'fs';
 
-const filePath: string = 'AdventOfCodeDay5/AOC5.txt';
+const filePath: string = 'AdventOfCodeDay5/AOC5_input.txt';
 
 const content: string = fs.readFileSync(filePath, 'utf-8')
-const lines: string[] = content.split(/\r?\n/);
 
-function inRange(value: number, min: number, max: number): boolean {
-  return value >= min && value <= max;
+const numberRegex = new RegExp('^(\\d+)$');
+const rangeRegex = new RegExp('^(\\d+)-(\\d+)$')
+
+function inRange(value: number, range: string): boolean {
+    const min = parseInt(range.split("-")[0])
+    const max = parseInt(range.split("-")[1])
+    return value >= min && value <= max;
 }
 
-let ranges = []
-let ids = []
-let reached_ids = false
-for (const line of lines) {
-    if (line == "") {
-        reached_ids = true
-    } else {
-        if (reached_ids) {
-            ids.push(line)
-        } else {
-            ranges.push([line.split("-")[0],line.split("-")[1]])
-        }
-    }
+const day5part1 = (input: string): number => { 
+    const ranges = input.split(/\r?\n/).filter(x => rangeRegex.test(x) == true)
+    const availableIds = input.split(/\r?\n/).filter(x => numberRegex.test(x) == true)
+    
+    return availableIds
+    // Checks each available ID against each range
+    .map(x => 
+        ranges.map(y => inRange(parseInt(x), y))
+    // Removes all falses from each array, meaning the value did not fit in that corresponding range 
+    .filter(x => x == true))
+    // Only keeps arrays with at least one true in them (implying the value corresponding to that array is in a fresh range)
+    .filter(x => x.length > 0)
+    // Counts the ID's that are in at least one range
+    .length
 }
 
-let fresh_id_count = 0
-for (const id of ids) {
-    for (const range of ranges) {
-        if (inRange(parseInt(id),parseInt(range[0]!),parseInt(range[1]!))) {
-            fresh_id_count++
-            break
-        }
-    }
-}
-
-console.log(fresh_id_count)
+console.log(day5part1(content))
