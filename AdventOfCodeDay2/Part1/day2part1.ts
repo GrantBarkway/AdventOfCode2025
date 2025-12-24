2dd0ceadcb64d5b0e3321eaefbf15301cb7fef6b
Grant Barkway <grantbarkway@shaw.ca>
2025-12-23 22:07:32 -0800

    Implemented suggestions for improvement.

--- AdventOfCodeDay2/Part1/day2part1.ts
+++ AdventOfCodeDay2/Part1/day2part1.ts
@@ -6,18 +6,21 @@ const content: string = fs.readFileSync(filePath, 'utf-8')
 // Regex for numbers repeated exactly twice
 const repeatedNumberRegex = new RegExp('^(\\d+)\\1{1}$');
 
-const day2part1 = (input: string): number =>{
-    const sum_invalid_ids = content.split(",")
-    .map(range => [Number(range.split("-")[0]), Number(range.split("-")[1])])
+const day2part1 = (input: string): number =>
+    input.split(",")
+    .map(range => {
+        const split_range = range.split("-")
+        return [parseInt(split_range[0]!), parseInt(split_range[1]!)]
+    })
     // Creates an array of all values in the range
-    .map(start => Array.from(Array(start[1]! - start[0]! + 1),(x,i)=>i + start[0]!))
-    // Puts all values in nested arrays into one single array
-    .flat(1)
+    .flatMap(([start, end]) => 
+        Array.from(
+            Array(end! - start! + 1),(_,i)=> i + start!
+        )
+    )
     // Removes all ID's that are not invalid
     .filter(x => repeatedNumberRegex.test(x.toString()))
-    // Sums all unvalid ID's
+    // Sums all invalid ID's
     .reduce((a,b) => a + b, 0)
-    return sum_invalid_ids
-}
 
 console.log("Sum of invalid ids: ", day2part1(content))
--- AdventOfCodeDay5/Part1/day5part1.ts
+++ AdventOfCodeDay5/Part1/day5part1.ts
@@ -8,21 +8,26 @@ const numberRegex = new RegExp('^(\\d+)$');
 const rangeRegex = new RegExp('^(\\d+)-(\\d+)$')
 
 function inRange(value: number, range: string): boolean {
-    const min = parseInt(range.split("-")[0])
-    const max = parseInt(range.split("-")[1])
-    return value >= min && value <= max;
+    const split_range = range.split("-")
+    return value >= parseInt(split_range[0]!) && value <= parseInt(split_range[1]!);
 }
 
 const day5part1 = (input: string): number => { 
-    const ranges = input.split(/\r?\n/).filter(x => rangeRegex.test(x) == true)
-    const availableIds = input.split(/\r?\n/).filter(x => numberRegex.test(x) == true)
-    
-    return availableIds
+    const lines = input.split(/\r?\n/)
+    return lines
+    .filter(x => numberRegex.test(x))
     // Checks each available ID against each range
     .map(x => 
-        ranges.map(y => inRange(parseInt(x), y))
-    // Removes all falses from each array, meaning the value did not fit in that corresponding range 
-    .filter(x => x == true))
+        lines
+        .filter(x => 
+            rangeRegex.test(x)
+        )
+        .map(y => 
+            inRange(parseInt(x), y)
+        )
+        // Removes all falses from each array, meaning the value did not fit in that corresponding range 
+        .filter(x => x)
+    )
     // Only keeps arrays with at least one true in them (implying the value corresponding to that array is in a fresh range)
     .filter(x => x.length > 0)
     // Counts the ID's that are in at least one range
