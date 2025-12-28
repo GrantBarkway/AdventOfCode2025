cc0f6198847e500a032b3a64ef5a956c881f16ac
Grant Barkway <grantbarkway@shaw.ca>
2025-12-27 20:41:48 -0800

    Wrote day 7 part 1 functionally.

--- AdventOfCodeDay7/Part1/day7part1.ts
+++ AdventOfCodeDay7/Part1/day7part1.ts
@@ -6,8 +6,40 @@ const content: string = fs.readFileSync(filePath, 'utf-8')
 
 const day7part1 = (input: string): number => {
     const lines = input.split(/\r?\n/)
-    console.log(lines)
-    return 0
+    
+    return lines
+        // The last element is all .'s, this removes it for efficiency
+        .slice(0,lines.length - 1)
+        // Gets the path the tachyon took by generating the computed state for an input that is truncated at each level. This was my only idea on how to write this purely functionally.
+        .map((_,i) => lines
+            .slice(1,i+2)
+                .reduce((cur, next) => cur
+                    .split("")
+                    // Computes and reassigns the "state" of each row, row by row
+                    .map((_,i) => {
+                        // Splits the next line to do operations on individual cells
+                        const split_next = next.split("")
+                        // booleans for if the cell at position i in the current row needs to be updated
+                        const next_cell_split = split_next[i+1] === "^" && (cur[i+1] === "|" || cur[i+1] === "S")
+                        const current_cell_downwards = split_next[i] === "." && (cur[i] === "S" || cur[i] ==="|")
+                        const prev_cell_split = split_next[i-1] === "^" && (cur[i-1] === "|" || cur[i-1] === "S")
+                        const next_is_split = split_next[i] === "^" && cur[i] === "|"
+                        
+                        return next_cell_split || prev_cell_split || current_cell_downwards || next_is_split
+                            ? next_is_split
+                                ? "^"
+                                : "|"
+                            : "."
+                    })
+        .join(""),
+    lines[0]))
+        // Removes all rows that do not contain "^"
+        .map(x => x
+            .split("")
+            .filter(y => y === "^"))
+        .filter(x => x.length > 0)
+        // Sums the total number of splits
+        .reduce((a,b) => a + b.length, 0)
 }
 
 console.log(day7part1(content))
