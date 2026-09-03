0ceede1faf1a102e8bce0e052db7f97677a347d6
Grant Barkway <grantbarkway@shaw.ca>
2026-09-02 20:14:24 -0700

    Updated day3part1 to remove errant for loop (not functional).

--- AdventOfCodeDay3/day3part1.ts
+++ AdventOfCodeDay3/day3part1.ts
@@ -2,28 +2,19 @@ import * as fs from 'fs';
 
 const filePath: string = 'AdventOfCodeDay3/AOC3_input.txt';
     
-const content: string = fs.readFileSync(filePath, 'utf-8')
-const lines: string[] = content.split(/\r?\n/);
+const content: string = fs.readFileSync(filePath, 'utf-8');
 
-let total_output = 0
-
-for (const line of lines) {
-    let first_digit = 0
-    let second_digit = 0
-    if (line != "") {
-        for (let i = 0; i < line.length; i++) {
-            if ((Number(line[i]) > first_digit) && (i < line.length - 1)) {
-                first_digit = Number(line[i])
-                second_digit = 0
-            } else {
-                if (Number(line[i]) > second_digit) {
-                    second_digit = Number(line[i])
-                }
-            }
-        }
-    }
-    let bank_jolts = Number(String(first_digit) + String(second_digit))
-    total_output += bank_jolts
+const day3part1 = (input: string) => {
+    const lines = input.split(/\r?\n/);
+    
+    return lines.map(line => {
+        const line_array = line.split("").map(x => Number(x))
+        // Gets index of largest value from the first n-1 spots
+        const first_index = line_array.indexOf(Math.max(...line_array.slice(0,-1)));
+        // Gets index of largest value from the remaining spots that follow
+        const second_index = line_array.indexOf(Math.max(...line_array.slice(first_index+1)))
+        return Number(String(line[first_index]) + String(line[second_index]))
+    }).reduce((a,b) => a + b, 0)
 }
 
-console.log(total_output)
+console.log(day3part1(content))
