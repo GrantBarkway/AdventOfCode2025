f9bd13478f72f564f1174d93b4b851285761bd97
Grant Barkway <grantbarkway@shaw.ca>
2026-09-02 20:44:55 -0700

    Updated day3part2 to remove errant for loop (not functional).

--- AdventOfCodeDay3/day3part2.ts
+++ AdventOfCodeDay3/day3part2.ts
@@ -1,26 +1,52 @@
 import * as fs from 'fs';
 
 const filePath: string = 'AdventOfCodeDay3/AOC3_input.txt';
+    
+const content: string = fs.readFileSync(filePath, 'utf-8');
 
-const content: string = fs.readFileSync(filePath, 'utf-8')
-const lines: string[] = content.split(/\r?\n/);
+const day3part2 = (input: string) => {
+    const lines = input.split(/\r?\n/);
+    
+    return lines.map(line => {
+        const line_array = line.split("").map(x => Number(x))
+        // Gets index of largest value from the first n-11 spots
+        const first_index = line_array.indexOf(Math.max(...line_array.slice(0,-11)));
+        // Gets index of largest value from the remaining spots that follow
+        const second_index_candidates = line_array.slice(first_index+1,-10)
+        const second_index = first_index + second_index_candidates.indexOf(Math.max(...second_index_candidates)) + 1
+        
+        const third_index_candidates = line_array.slice(second_index+1,-9)
+        const third_index = second_index + third_index_candidates.indexOf(Math.max(...third_index_candidates)) + 1
 
-let total_output = 0
+        const fourth_index_candidates = line_array.slice(third_index+1,-8)
+        const fourth_index = third_index + fourth_index_candidates.indexOf(Math.max(...fourth_index_candidates)) + 1
 
-for (const line of lines) {
-    let bank_jolts = line.slice(0,12).split("")
-    for (let i = 12; i < line.length; i++) {
-        for (let j = 0; j <  bank_jolts.length; j++) {
-            let bank_jolts_copy = JSON.parse(JSON.stringify(bank_jolts))
-            bank_jolts_copy.splice(j,1)
-            bank_jolts_copy.push(String(line[i]))
-            if (Number(bank_jolts_copy.join("")) > Number(bank_jolts.join(""))) {
-                bank_jolts = bank_jolts_copy
-                break
-            }
-        }
-    }
-    total_output += Number(bank_jolts.join(""))
+        const fifth_index_candidates = line_array.slice(fourth_index+1,-7)
+        const fifth_index = fourth_index + fifth_index_candidates.indexOf(Math.max(...fifth_index_candidates)) + 1
+
+        const sixth_index_candidates = line_array.slice(fifth_index+1,-6)
+        const sixth_index = fifth_index + sixth_index_candidates.indexOf(Math.max(...sixth_index_candidates)) + 1
+
+        const seventh_index_candidates = line_array.slice(sixth_index+1,-5)
+        const seventh_index = sixth_index + seventh_index_candidates.indexOf(Math.max(...seventh_index_candidates)) + 1
+        
+        const eighth_index_candidates = line_array.slice(seventh_index+1,-4)
+        const eighth_index = seventh_index + eighth_index_candidates.indexOf(Math.max(...eighth_index_candidates)) + 1
+        
+        const ninth_index_candidates = line_array.slice(eighth_index+1,-3)
+        const ninth_index = eighth_index + ninth_index_candidates.indexOf(Math.max(...ninth_index_candidates)) + 1
+
+        const tenth_index_candidates = line_array.slice(ninth_index+1,-2)
+        const tenth_index = ninth_index + tenth_index_candidates.indexOf(Math.max(...tenth_index_candidates)) + 1
+        
+        const eleventh_index_candidates = line_array.slice(tenth_index+1,-1)
+        const eleventh_index = tenth_index + eleventh_index_candidates.indexOf(Math.max(...eleventh_index_candidates)) + 1
+        
+        const twelvth_index_candidates = line_array.slice(eleventh_index+1)
+        const twelvth_index = eleventh_index + twelvth_index_candidates.indexOf(Math.max(...twelvth_index_candidates)) + 1
+        
+        return Number(String(line[first_index]) + String(line[second_index]) + String(line[third_index]) + String(line[fourth_index]) + String(line[fifth_index]) + String(line[sixth_index]) + String(line[seventh_index]) + String(line[eighth_index]) + String(line[ninth_index]) + String(line[tenth_index]) + String(line[eleventh_index]) + String(line[twelvth_index]))
+    }).reduce((a,b) => a + b, 0)
 }
 
-console.log(total_output)
+console.log(day3part2(content))
